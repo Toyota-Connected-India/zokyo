18bcffa29671d84697b5948161b4e0f86c2c9a23
Vi-Sri <nikola.tesla195@gmail.com>
2020-11-09 17:21:36 +0530

    adding operations

--- sphinx/augmentation/operations.py
+++ sphinx/augmentation/operations.py
@@ -18,7 +18,7 @@ class ArgsClass(object):
         self.__dict__.update((key, kwargs[key]) for key in kwargs)
 
 
-class ColorEqualize(Operation):
+class EqualizeScene(Operation):
     def __init__(self, **kwargs):
         args = ArgsClass(**kwargs)
         Operation.__init__(self, args.probability)
@@ -28,7 +28,7 @@ class ColorEqualize(Operation):
             with warnings.catch_warnings():
                 warnings.simplefilter("ignore")
                 return ImageOps.equalize(image)
-
+    
         augmented_images = []
         for image in images:
             augmented_images.append(do(image))
@@ -66,3 +66,71 @@ class DarkenScene(Operation):
         for image in images:
             augmented_images.append(do(image))
         return augmented_images
+
+# TODO : Add Sky augmentation operation
+
+# TODO : Add a common util to change lighting instead of repeating the implementation
+class BrightenScene(Operation):
+    def __init__(self, **kwargs):
+        args = ArgsClass(**kwargs)
+        Operation.__init__(self, args.probability)
+
+        if args.coefficient is None:
+            raise CrucialValueNotFoundError(
+                "BrightenScene", sample_type="coefficient")
+
+        if (args.coefficient != -1):
+            if (args.coefficient < 0.0 or args.coefficient > 1.0):
+                raise CoefficientNotinRangeError(
+                    args.coefficient, "BrightnessCoefficient", 0, 1)
+        self.brightness_coeff = 1 + args.coefficient
+
+    def perform_operation(self, images):
+
+        def do(image):
+            image = np.array(image, dtype=np.uint8)
+            image_HLS = cv2.cvtColor(image, cv2.COLOR_RGB2HLS)
+            image_HLS[:, :, 1] = image_HLS[:, :, 1] * self.brightness_coeff
+            image_HLS[:, :, 1][image_HLS[:, :, 1] > 255] = 255
+            image_HLS[:, :, 1][image_HLS[:, :, 1] < 0] = 0
+            image_HLS = np.array(image_HLS, dtype=np.uint8)
+            image_RGB = cv2.cvtColor(image_HLS, cv2.COLOR_HLS2RGB)
+            return Image.fromarray(image_RGB)
+
+        augmented_images = []
+        for image in images:
+            augmented_images.append(do(image))
+        return augmented_images 
+
+class RandomBrightness(Operation):
+    def __init__(self, **kwargs):
+        args = ArgsClass(**kwargs)
+        Operation.__init__(self, args.probability)
+
+        if args.distribution is None:
+            raise CrucialValueNotFoundError(
+                "RandomBrightness", sample_type="coefficient")
+
+        if args.distribution == "normal":
+            self.coeff = 2 * np.random.normal(0,1)
+        elif args.distribution == "uniform":
+            self.coeff = 2 * np.random.uniform(0,1)
+
+    def perform_operation(self, images):
+
+        def do(image):
+            image = np.array(image, dtype=np.uint8)
+            image_HLS = cv2.cvtColor(image, cv2.COLOR_RGB2HLS)
+            image_HLS[:, :, 1] = image_HLS[:, :, 1] * self.coeff
+            image_HLS[:, :, 1][image_HLS[:, :, 1] > 255] = 255
+            image_HLS[:, :, 1][image_HLS[:, :, 1] < 0] = 0
+            image_HLS = np.array(image_HLS, dtype=np.uint8)
+            image_RGB = cv2.cvtColor(image_HLS, cv2.COLOR_HLS2RGB)
+            return Image.fromarray(image_RGB)
+
+        augmented_images = []
+        for image in images:
+            augmented_images.append(do(image))
+        return augmented_images
+
+
--- sphinx/augmentation/utils.py
+++ sphinx/augmentation/utils.py
@@ -1,2 +1,196 @@
 import cv2
 import numpy as np
+from scipy import spatial
+
+def make_mask(self, b, image):
+    mask = np.zeros((image.shape[0], image.shape[1], 1), dtype=np.uint8)
+    for xx, yy in enumerate(b):
+        mask[yy:, xx] = 255
+    return mask
+
+
+def display_mask(self, b, image, color=[0, 0, 255]):
+    result = image.copy()
+    overlay = np.full(image.shape, color, image.dtype)
+    display(
+        cv2.addWeighted(
+            cv2.bitwise_and(overlay, overlay, mask=make_mask(b, image)),
+            1,
+            image,
+            1,
+            0,
+            result
+        )
+    )
+
+def color_to_gradient(image):
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    return np.hypot(
+        cv2.Sobel(gray, cv2.CV_64F, 1, 0),
+        cv2.Sobel(gray, cv2.CV_64F, 0, 1)
+    )
+
+def energy(b_tmp, image):
+    sky_mask = make_mask(b_tmp, image)
+
+    ground = np.ma.array(
+        image,
+        mask=cv2.cvtColor(cv2.bitwise_not(sky_mask), cv2.COLOR_GRAY2BGR)
+    ).compressed()
+    sky = np.ma.array(
+        image,
+        mask=cv2.cvtColor(sky_mask, cv2.COLOR_GRAY2BGR)
+    ).compressed()
+    ground.shape = (ground.size//3, 3)
+    sky.shape = (sky.size//3, 3)
+
+    sigma_g, mu_g = cv2.calcCovarMatrix(
+        ground,
+        None,
+        cv2.COVAR_NORMAL | cv2.COVAR_ROWS | cv2.COVAR_SCALE
+    )
+    sigma_s, mu_s = cv2.calcCovarMatrix(
+        sky,
+        None,
+        cv2.COVAR_NORMAL | cv2.COVAR_ROWS | cv2.COVAR_SCALE
+    )
+
+    y = 2
+
+    return 1 / (
+        (y * np.linalg.det(sigma_s) + np.linalg.det(sigma_g)) +
+        (y * np.linalg.det(np.linalg.eig(sigma_s)[1]) +
+            np.linalg.det(np.linalg.eig(sigma_g)[1]))
+    )
+
+def calculate_border(grad, t):
+    sky = np.full(grad.shape[1], grad.shape[0])
+
+    for x in range(grad.shape[1]):
+        border_pos = np.argmax(grad[:, x] > t)
+
+        # argmax hax return 0 if nothing is > t
+        if border_pos > 0:
+            sky[x] = border_pos
+
+    return sky
+
+
+def calculate_border_optimal(image, thresh_min=5, thresh_max=600, search_step=5):
+    grad = color_to_gradient(image)
+
+    n = ((thresh_max - thresh_min) // search_step) + 1
+
+    b_opt = None
+    jn_max = 0
+
+    for k in range(1, n + 1):
+        t = thresh_min + ((thresh_max - thresh_min) // n - 1) * (k - 1)
+
+        b_tmp = calculate_border(grad, t)
+        jn = energy(b_tmp, image)
+
+        if jn > jn_max:
+            jn_max = jn
+            b_opt = b_tmp
+
+    return b_opt
+
+def no_sky_region(bopt, thresh1, thresh2, thresh3):
+    border_ave = np.average(bopt)
+    asadsbp = np.average(np.absolute(np.diff(bopt)))
+
+    return border_ave < thresh1 or (border_ave < thresh2 and asadsbp > thresh3)
+
+def partial_sky_region(bopt, thresh4):
+    return np.any(np.diff(bopt) > thresh4)
+
+def refine_sky(bopt, image):
+    sky_mask = make_mask(bopt, image)
+
+    ground = np.ma.array(
+        image,
+        mask=cv2.cvtColor(cv2.bitwise_not(sky_mask), cv2.COLOR_GRAY2BGR)
+    ).compressed()
+    sky = np.ma.array(
+        image,
+        mask=cv2.cvtColor(sky_mask, cv2.COLOR_GRAY2BGR)
+    ).compressed()
+    ground.shape = (ground.size//3, 3)
+    sky.shape = (sky.size//3, 3)
+
+    ret, label, center = cv2.kmeans(
+        np.float32(sky),
+        2,
+        None,
+        (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0),
+        10,
+        cv2.KMEANS_RANDOM_CENTERS
+    )
+
+    sigma_s1, mu_s1 = cv2.calcCovarMatrix(
+        sky[label.ravel() == 0],
+        None,
+        cv2.COVAR_NORMAL | cv2.COVAR_ROWS | cv2.COVAR_SCALE
+    )
+    ic_s1 = cv2.invert(sigma_s1, cv2.DECOMP_SVD)[1]
+
+    sigma_s2, mu_s2 = cv2.calcCovarMatrix(
+        sky[label.ravel() == 1],
+        None,
+        cv2.COVAR_NORMAL | cv2.COVAR_ROWS | cv2.COVAR_SCALE
+    )
+    ic_s2 = cv2.invert(sigma_s2, cv2.DECOMP_SVD)[1]
+
+    sigma_g, mu_g = cv2.calcCovarMatrix(
+        ground,
+        None,
+        cv2.COVAR_NORMAL | cv2.COVAR_ROWS | cv2.COVAR_SCALE
+    )
+    icg = cv2.invert(sigma_g, cv2.DECOMP_SVD)[1]
+
+    if cv2.Mahalanobis(mu_s1, mu_g, ic_s1) > cv2.Mahalanobis(mu_s2, mu_g, ic_s2):
+        mu_s = mu_s1
+        sigma_s = sigma_s1
+        ics = ic_s1
+    else:
+        mu_s = mu_s2
+        sigma_s = sigma_s2
+        ics = ic_s2
+
+    for x in range(image.shape[1]):
+        cnt = np.sum(np.less(
+            spatial.distance.cdist(
+                image[0:bopt[x], x],
+                mu_s,
+                'mahalanobis',
+                VI=ics
+            ),
+            spatial.distance.cdist(
+                image[0:bopt[x], x],
+                mu_g,
+                'mahalanobis',
+                VI=icg
+            )
+        ))
+
+        if cnt < (bopt[x] / 2):
+            bopt[x] = 0
+
+    return bopt
+
+def detect_sky(image):
+    display(input_image)
+
+    bopt = calculate_border_optimal(image)
+
+    if no_sky_region(bopt, image.shape[0]/30, image.shape[0]/4, 5):
+        display("No sky detected")
+        return
+    
+    display_mask(bopt, image)
+
+    if partial_sky_region(bopt, image.shape[1]/3):
+        bnew = refine_sky(bopt, image)
+    
+        display_mask(bnew, image)
