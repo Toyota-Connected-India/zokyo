2ae7d51495f6c72c5c4a1471ab8b4eb933958a66
harshav7 <harshaiitm1@gmail.com>
2020-12-09 15:35:07 +0530

    sun flare

--- sphinx/augmentation/operations.py
+++ sphinx/augmentation/operations.py
@@ -13,7 +13,7 @@ import numpy as np
 import random
 from random import randint
 import warnings
-
+import math
 
 class ArgsClass(object):
     def __init__(self, **kwargs):
@@ -444,3 +444,74 @@ class FogScene(Operation):
 
     def perform_operation(self, images):
         pass
+
+
+class SunFlare(Operation):
+    def __init__(self, **kwargs):
+        args = ArgsClass(**kwargs)
+        Operation.__init__(self, args.probability)
+
+    def perform_operation(self, images):
+        def flare_source(image, point,radius,src_color):
+            overlay= image.copy()
+            output= image.copy()
+            num_times=radius//10
+            alpha= np.linspace(0.0,1,num= num_times)
+            rad= np.linspace(1,radius, num=num_times)
+            for i in range(num_times):
+                cv2.circle(overlay,point, int(rad[i]), src_color, -1)
+                alp=alpha[num_times-i-1]*alpha[num_times-i-1]*alpha[num_times-i-1]
+                cv2.addWeighted(overlay, alp, output, 1 -alp ,0, output)
+            return output
+
+        def add_sun_flare_line(flare_center,angle,imshape):
+            x=[]
+            y=[]
+            i=0
+            for rand_x in range(0,imshape[1],10):
+                rand_y= math.tan(angle)*(rand_x-flare_center[0])+flare_center[1]
+                x.append(rand_x)
+                y.append(2*flare_center[1]-rand_y)
+            return x,y
+
+        def add_sun_process(image, no_of_flare_circles,flare_center,src_radius,x,y,src_color):
+            overlay= image.copy()
+            output= image.copy()
+            imshape=image.shape
+            for i in range(no_of_flare_circles):
+                alpha=random.uniform(0.05,0.2)
+                r=random.randint(0, len(x)-1)
+                rad=random.randint(1, imshape[0]//100-2)
+                cv2.circle(overlay,(int(x[r]),int(y[r])), rad*rad*rad, (random.randint(max(src_color[0]-50,0), src_color[0]),random.randint(max(src_color[1]-50,0), src_color[1]),random.randint(max(src_color[2]-50,0), src_color[2])), -1)
+                cv2.addWeighted(overlay, alpha, output, 1 - alpha,0, output)                      
+            output= flare_source(output,(int(flare_center[0]),int(flare_center[1])),src_radius,src_color)
+            return output
+
+        def add_sun_flare(image,flare_center=-1, angle=-1, no_of_flare_circles=3,src_radius=100, src_color=(255,255,255)):
+            image = np.array(image, dtype=np.uint8)
+            imshape=image.shape
+            if(angle==-1):
+                angle_t=random.uniform(0,2*math.pi)
+                if angle_t==math.pi/2:
+                    angle_t=0
+            else:
+                angle_t=angle
+            if flare_center==-1:
+                flare_center_t=(random.randint(0,imshape[1]),random.randint(0,imshape[0]//2))
+            else:
+                flare_center_t=flare_center
+            x,y= add_sun_flare_line(flare_center_t,angle_t,imshape)
+            output= add_sun_process(image, no_of_flare_circles,flare_center_t,src_radius,x,y,src_color)
+            image_RGB = output
+            return image_RGB
+
+        def do(images):
+            if len(images) > 1:
+                return [Image.fromarray(add_sun_flare(images[0])), images[1]]
+            else:
+                return [Image.fromarray(add_sun_flare(images[0]))]
+
+        return do(images)
+
+        
+    
