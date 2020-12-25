6059cadd9a9bca2277bd2565a36f113bbfcfc872
Vi-Sri <nikola.tesla195@gmail.com>
2020-12-25 18:43:25 +0530

    master fixes

--- sphinx/augmentation/__init__.py
+++ sphinx/augmentation/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-__all__ = ('augment', 'operations', 'utils', 'pipeline', 'generators')
+__all__ = ('augment', 'operations', 'utils', 'pipeline', 'generators', 'data')
 
 # To support old imports
 from .augment import *
@@ -8,3 +8,4 @@ from .operations import *
 from .utils import *
 from .pipeline import *
 from .generators import *
+from .data import *
